859e458d7c3e2440552791ad70377fbf41a735f0
abubakar-sadiq-001 <abubakarsadiqi532@gmail.com>
2025-12-03 16:44:50 +0100

    Initial commit

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,4 +1,4 @@
-const { createClient } = require("@supabase/supabase-js")
+import { createClient } from "@supabase/supabase-js"
 
 export const supaBaseUrl = "https://owryejktncbewshnrtva.supabase.co"
 

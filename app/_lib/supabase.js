57dfc0718346b914868852667ed89c617ec8b1c8
abubakar-sadiq-001 <abubakarsadiqi532@gmail.com>
2025-12-03 15:37:20 +0100

    Initial commit

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -3,6 +3,6 @@ const { createClient } = require("@supabase/supabase-js")
 export const supaBaseUrl = "https://owryejktncbewshnrtva.supabase.co"
 
 export const supabase = createClient(
-  "https://owryejktncbewshnrtva.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cnllamt0bmNiZXdzaG5ydHZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MzEyNzEsImV4cCI6MjA3NjAwNzI3MX0.kRVIEfHIv7v6UpsM7FqcR5WWJGle_ZYsZ7E6Wap-i3M"
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )

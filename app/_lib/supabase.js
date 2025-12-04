ec1875a0e2fac7aefa7887ae2c42e035cd408449
abubakar-sadiq-001 <abubakarsadiqi532@gmail.com>
2025-12-04 11:16:58 +0100

    Initial commit

--- app/_lib/supabase.js
+++ app/_lib/supabase.js
@@ -1,17 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-// export const supaBaseUrl = "https://owryejktncbewshnrtva.supabase.co"
+export const supaBaseUrl = "https://owryejktncbewshnrtva.supabase.co"
 
-// export const supabase = createClient(
-//   process.env.NEXT_PUBLIC_SUPABASE_URL,
-//   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-// )
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_SERVICE_ROLE_KEY
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables")
-}
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  "https://owryejktncbewshnrtva.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93cnllamt0bmNiZXdzaG5ydHZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MzEyNzEsImV4cCI6MjA3NjAwNzI3MX0.kRVIEfHIv7v6UpsM7FqcR5WWJGle_ZYsZ7E6Wap-i3M"
+)
